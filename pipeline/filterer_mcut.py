ee79ebab48f9ef27d45eb4e229f424d301ed878c
chervias <aconcagua.chc@gmail.com>
2023-10-31 07:52:08 -0700

    Fixes to the pcler and the filterer

--- pipeline/filterer_mcut.py
+++ pipeline/filterer_mcut.py
@@ -26,40 +26,41 @@ def filter_map(man, mpQ, mpU, msk, m_cut):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Simple m-cut filter')
-    parser.add_argument("--globals", type=str,
-                        help='Path to yaml with global parameters')
-    parser.add_argument("--first-sim", type=int, help='Index of first sim')
-    parser.add_argument("--num-sims", type=int, help='Number of sims')
-    parser.add_argument("--sim-sorter", type=str,
-                        help='Name of sorting routine')
-    parser.add_argument("--m-cut", type=int, help='m-mode cut')
-    parser.add_argument("--output-dir", type=str, help='Output directory')
-    parser.add_argument("--plot", action='store_true',
-                        help='Show last filtered and unfiltered maps?')
-    o = parser.parse_args()
-    man = PipelineManager(o.globals)
+	parser = argparse.ArgumentParser(description='Simple m-cut filter')
+	parser.add_argument("--globals", type=str,
+						help='Path to yaml with global parameters')
+	parser.add_argument("--first-sim", type=int, help='Index of first sim')
+	parser.add_argument("--num-sims", type=int, help='Number of sims')
+	parser.add_argument("--sim-sorter", type=str,
+						help='Name of sorting routine')
+	parser.add_argument("--m-cut", type=int, help='m-mode cut')
+	parser.add_argument("--output-dir", type=str, help='Output directory')
+	parser.add_argument("--plot", action='store_true',
+						help='Show last filtered and unfiltered maps?')
+	o = parser.parse_args()
+	man = PipelineManager(o.globals)
 
-    sorter = getattr(man, o.sim_sorter)
-    file_input_list = sorter(o.first_sim, o.num_sims, o.output_dir,
-                             which='input')
-    file_output_list = sorter(o.first_sim, o.num_sims, o.output_dir,
-                              which='filtered')
+	sorter = getattr(man, o.sim_sorter)
+	file_input_list = sorter(o.first_sim, o.num_sims, o.output_dir,
+							 which='input')
+	file_output_list = sorter(o.first_sim, o.num_sims, o.output_dir,
+							  which='filtered')
 
-    msk = hp.read_map(man.fname_binary_mask)
-    for fin, fout in zip(file_input_list, file_output_list):
-        # Read maps
-        mpQ, mpU = hp.read_map(fin, field=[1,2]) # this is because now maps have to be TQU
-        # Filter
-        mpQf, mpUf = filter_map(man, mpQ, mpU, msk, o.m_cut)
-        # Write output
-        hp.write_map(fout, [mpQf, mpUf], overwrite=True, dtype=np.float32)
+	msk = hp.read_map(man.fname_binary_mask)
+	for fin, fout in zip(file_input_list, file_output_list):
+		# Read maps
+		mpQ, mpU = hp.read_map(fin, field=[1,2]) # this is because now maps have to be TQU
+		# Filter
+		mpQf, mpUf = filter_map(man, mpQ, mpU, msk, o.m_cut)
+		# Write output
+		mpTf = np.zeros(12*man.nside**2)
+		hp.write_map(fout, [mpTf, mpQf, mpUf], overwrite=True, dtype=np.float32)
 
-    if o.plot:
-        import matplotlib.pyplot as plt
+	if o.plot:
+		import matplotlib.pyplot as plt
 
-        hp.mollview(mpQ, title='Unfiltered Q')
-        hp.mollview(mpQf, title='Filtered Q')
-        hp.mollview(mpU, title='Unfiltered U')
-        hp.mollview(mpUf, title='Filtered U')
-        plt.show()
+		hp.mollview(mpQ, title='Unfiltered Q')
+		hp.mollview(mpQf, title='Filtered Q')
+		hp.mollview(mpU, title='Unfiltered U')
+		hp.mollview(mpUf, title='Filtered U')
+		plt.show()
